917e0a98cfec701a5c2474bffbe190582d847c39
Nauman patel <Nauman0504@gmail.com>
2026-07-18 00:57:56 +0100

    Add: is-anagram - submission-3

---
+++ Data Structures & Algorithms/is-anagram/submission-3.py
@@ -0,0 +1,17 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        if len(s) != len(t):
+            return False
+        
+        s_hash = {}
+        t_hash = {}
+
+        for char in s:
+            s_hash[char] = s_hash.get(char,0) + 1
+        for char in t:
+            t_hash[char] = t_hash.get(char,0) + 1
+
+        if s_hash == t_hash:
+            return True
+        else:
+            return False
