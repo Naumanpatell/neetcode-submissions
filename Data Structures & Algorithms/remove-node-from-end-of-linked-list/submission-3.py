2e68d1ce0bb284cc2cc01b42ccf07a0f763307b6
Nauman patel <Nauman0504@gmail.com>
2026-07-25 00:53:04 +0100

    Add: remove-node-from-end-of-linked-list - submission-3

---
+++ Data Structures & Algorithms/remove-node-from-end-of-linked-list/submission-3.py
@@ -0,0 +1,51 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+
+class Solution:
+    def removeNthFromEnd(self, head: Optional[ListNode], n: int) -> Optional[ListNode]:
+        # Method 2 --> Claude ans
+
+        dummy = ListNode(0,head)
+        slow = dummy
+        fast = dummy
+
+        for _ in range(n+1):
+            fast = fast.next
+        
+        while fast:
+            slow = slow.next
+            fast = fast.next
+
+        slow.next = slow.next.next
+        return dummy.next
+        # Method 1 (Reverse, find, remove, Re-reverse) --> My first thought
+
+        # prev = None
+        # curr = head
+        # while curr:
+        #     nxt = curr.next
+        #     curr.next = prev
+        #     prev = curr
+        #     curr = nxt
+        # new_head = prev
+
+        # dummy = ListNode(0,new_head)
+        # pointer = dummy
+        # for _ in range(n-1):
+        #     pointer = pointer.next
+        # pointer.next = pointer.next.next
+
+        # prev = None
+        # curr = dummy.next
+        # while curr:
+        #     nxt = curr.next
+        #     curr.next = prev
+        #     prev = curr
+        #     curr = nxt
+        # return prev
+
+# Revised today -> Solved but forgot the fast = fast.next pointer in the second loop and got an error found it in the after the err. I HAVE TO WORK ON POINTERS
+# Revised on 25 July 2026 -> logically the code was 100% right excution me one error {Listnode(head,0) --> wrong}
